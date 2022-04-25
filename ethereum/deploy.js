9d14fe5335757a08728e64e78c7cfae70f0d0e70
Ángel Sánchez <mektigboy@protonmail.com>
2022-04-25 16:57:07 +0000

    Copy & paste Crowdfunding Smart Contract

---
+++ ethereum/deploy.js
@@ -0,0 +1,29 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const compiledFactory = require('./build/CampaignFactory.json');
+const provider = new HDWalletProvider(
+  // This seed phrase is from a test wallet.
+  // Use environment variables when using a real wallet.
+  'bachelor elder stairs salon duck follow disease local kid tube strike code',
+  'https://rinkeby.infura.io/v3/cbe42a9daef64613bff25927292e1666'
+);
+const web3 = new Web3(provider);
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from accounts', accounts[0]);
+
+  const result = await new web3.eth.Contract(
+    JSON.parse(compiledFactory.interface)
+  )
+    .deploy({ data: compiledFactory.bytecode })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  // console.log(compiledFactory.interface);
+  console.log('Contract deployed to', result.options.address);
+
+  //To prevent a hanging deployment.
+  provider.engine.stop();
+};
+
+deploy();
