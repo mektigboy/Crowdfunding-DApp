eed648dc0d914a28dca6fd7c7fd872c768992b8d
Ángel Sánchez <mektigboy@protonmail.com>
2022-04-25 19:03:12 +0000

    Update ethereum/web3.js

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -1,16 +1,16 @@
-import Web3 from "web3";
+import Web3 from 'web3';
 
 let web3;
 
-if (typeof window !== "undefined" && typeof window.ethereum !== "undefined") {
+if (typeof window !== 'undefined' && typeof window.ethereum !== 'undefined') {
   // Browser & Metamask running.
-  window.ethereum.request({ method: "eth_requestAccounts" });
-  
+  window.ethereum.request({ method: 'eth_requestAccounts' });
+
   web3 = new Web3(window.ethereum);
 } else {
   // Server or user is not running Metamask.
   const provider = new Web3.providers.HttpProvider(
-    "https://rinkeby.infura.io/v3/15c1d32581894b88a92d8d9e519e476c"
+    'https://rinkeby.infura.io/v3/cbe42a9daef64613bff25927292e1666'
   );
 
   web3 = new Web3(provider);
