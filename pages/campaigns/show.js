b56b11027c0cb1dae280b0866dac06b7b248d058
Ángel Sánchez <mektigboy@protonmail.com>
2022-05-01 02:55:24 +0000

    Create & update ethereum/campaign.js | Update pages/campaigns/show.js

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 import Layout from '../../components/Layout';
+import campaignFile from '../../ethereum/campaign';
 
 class CampaignShow extends Component {
   static async getInitialProps(props){
-    console.log(props.query.address);
+    const campaign = campaignFile(props.query.address);
+    const summary = await campaign.methods.getSummary().call();
+
+    console.log(summary);
+    
     return {};
   }
 
