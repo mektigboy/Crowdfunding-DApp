662aff7a58753da5a6ec91af8de140a7899e0bf5
Ángel Sánchez <mektigboy@protonmail.com>
2022-05-02 15:48:52 -0500

    Update components/ContributeForm.js

--- components/ContributeForm.js
+++ components/ContributeForm.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
+import { Form, Input, Message } from 'semantic-ui-react';
 
 class ContributeForm extends Component {
   render() {
-    
+    render (
+      <Form>
+        <Form.Field>
+          <label>Amount to contribute</label>
+          <Input
+            label='ether'
+            labelPosition='right'
+          />
+        </Form.Field>
+        <Button primary>
+          Contribute
+        </Button>
+      </Form>
+    )
   }
 }
 
