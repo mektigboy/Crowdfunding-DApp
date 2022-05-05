54d2d8799a7f777981af42c226eba163c43c179c
Ángel Sánchez <mektigboy@protonmail.com>
2022-05-05 13:52:45 -0500

    Update pages/campaigns/requests/new.js, routes.js

---
+++ pages/campaigns/requests/new.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Layout from '../../../components/Layout';
+
+class RequestNew extends Component {
+  render() {
+    return(
+      <Layout>
+      </Layout>
+    );
+  }
+}
+
+export default RequestNew;
