8805cbd3a0bb19b95e29341bf170a3bf9f1a2601
Ángel Sánchez <mektigboy@protonmail.com>
2022-05-05 10:45:07 -0500

    Update components/ContributeForm.js

--- components/ContributeForm.js
+++ components/ContributeForm.js
@@ -6,13 +6,17 @@ import { Router } from '../routes';
 
 class ContributeForm extends Component {
   state = {
-    value: ''
+    value: '',
+    errorMessage: '',
+    loading: false
   };
 
   onSubmit = async (event) => {
     event.preventDefault();
 
     const campaign = Campaign(this.props.address);
+
+    this.setState({ loading: true, errorMessage: '' });
 
     try {
       const accounts = await web3.eth.getAccounts();
@@ -24,12 +28,16 @@ class ContributeForm extends Component {
 
       Router.replaceRoute(`/campaigns/${this.props.address}`); // Refresh page with routes.
 
-    } catch (err) {}
+    } catch (err) {
+      this.setState({ errorMessage: err.message });
+    }
+
+    this.setState({ loading: false, value: '' });
   }
 
   render() {
     return (
-      <Form onSubmit={this.onSubmit}>
+      <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
         <Form.Field>
           <label>Amount to contribute</label>
           <Input
@@ -39,7 +47,9 @@ class ContributeForm extends Component {
             labelPosition='right'
           />
         </Form.Field>
-        <Button primary>
+
+        <Message error header='Oops!' content={this.state.errorMessage} />
+        <Button loading={this.state.loading} primary>
           Contribute
         </Button>
       </Form>
