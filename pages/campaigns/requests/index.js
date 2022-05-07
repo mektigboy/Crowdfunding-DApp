ae5ff1a42589b766d1cc121441c8863a587dfcdc
Ángel Sánchez <mektigboy@protonmail.com>
2022-05-07 16:23:52 -0500

    Update pages/campaigns/requests/index.js

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -40,6 +40,11 @@ class RequestIndex extends Component {
     return (
       <Layout>
         <h3>Requests</h3>
+        <Link route={`/campaigns/${this.props.address}/requests/new`}>
+          <a>
+            <Button primary floated='right' style={{ marginBottom: 10 }}>Add request</Button>
+          </a>
+        </Link>
         <Table>
           <Header>
             <Row>
@@ -54,11 +59,7 @@ class RequestIndex extends Component {
           </Header>
           <Body>{this.renderRows()}</Body>
         </Table>
-        <Link route={`/campaigns/${this.props.address}/requests/new`}>
-          <a>
-            <Button primary>Add request</Button>
-          </a>
-        </Link>
+        <div>Found {this.props.requestCount} requests.</div>
       </Layout>
     );
   }
