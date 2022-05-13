e6d041aedd3e8018703d1719cc9646ae74f0d13e
Ángel Sánchez <mektigboy@protonmail.com>
2022-05-13 17:47:39 -0500

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,6 @@ app.prepare().then(() => {
   createServer(handler).listen(3000), (err) => {
     if (err) throw err;
 
-    console.log('Ready on localhost:3000');
+    console.log('Ready on localhost: 3000');
   }
 });
